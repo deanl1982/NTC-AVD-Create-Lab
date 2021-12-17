be30b380de1cf5f40fe7832a8dc7606d73fd26ae
Dean Lawrence - Avanade <dal08082@gmail.com>
2021-12-17 08:37:22 +0000

    Updated Choco

--- install-choco.ps1
+++ install-choco.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-Wait 10
+# Wait 10
 
 choco install adobereader googlechrome 7zip.install notepadplusplus.install vlc putty.install --force -y
